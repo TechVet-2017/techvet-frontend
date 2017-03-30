2e3df341a89c64ba1519d6b79e289019b510050b
Luiz Guilherme Silva <do.guilherme@hotmail.com>
2017-03-30 00:34:48 -0300

    Add ESLint configuration files Fix travisCI build to continuous integration
    Signed-off-by: Luiz Guilherme Silva <do.guilherme@hotmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
+/* eslint-env browser */
+/* global document */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <h1>Hello, world</h1>,
-  document.getElementById('root')
+document.getElementById('root'),
 );
