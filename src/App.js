c9054c73bab7c080b9c162fd9e3ebcd0931a0b50
Lucas Amoêdo <lucas_advc@outlook.com>
2017-03-31 11:04:55 -0300

    Create application's header
    Related to issue #7
    
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const App = function App() {
+  return (
+    <div>
+      <Header />
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
