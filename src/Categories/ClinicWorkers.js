1e5c6ac73466637c53a78cdfbe751c1a362d4dd5
Luiz Guilherme Silva <do.guilherme@hotmail.com>
2017-04-07 19:10:07 -0300

    Add create, edit and filter to users Incomplete, but functional, formularies Related to issue: #9
    Signed-off-by: Luiz Guilherme Silva <do.guilherme@hotmail.com>

--- src/Categories/ClinicWorkers.js
+++ src/Categories/ClinicWorkers.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { List, Datagrid, EmailField, TextField } from 'admin-on-rest/lib/mui';
+import { List, Responsive, SimpleList, Edit, Create, Datagrid, EmailField, EmailInput, TextField, Filter, EditButton, DeleteButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput } from 'admin-on-rest/lib/mui';
+
+const UserFilter = props => (
+  <Filter {...props}>
+    <TextInput label="Pesquisar" source="q" alwaysOn />
+  </Filter>
+);
 
 export const UserList = props => (
-  <List {...props} title={'Usuários'}>
-    <Datagrid>
-      <TextField source="id" label="ID" />
-      <TextField source="name" label="Nome" />
-      <EmailField source="email" label="Email" />
-    </Datagrid>
+   <List {...props} filters={<UserFilter />} title={'Lista de Usuários'}>
+     <Responsive
+      small={
+          <SimpleList
+            primaryText={record => record.title}
+            secondaryText={record => `${record.views} views`}
+            tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+          />
+      }
+      medium={
+         <Datagrid>
+            <TextField source="id" label="ID" />
+            <TextField source="name" label="Nome" />
+            <EmailField source="email" label="Email" />
+            <EditButton />
+          </Datagrid>
+      }
+     />
   </List>
+);
+
+const UserName = ({ record }) => {
+  return <span>Usuário {record ? `"${record.name}"` : ''}</span>;
+};
+
+export const UserEdit = props => (
+  <Edit title={<UserName />} {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" label="ID"/>
+      <TextInput source="name" label="Nome"/>
+      <TextInput source="email" label="Email"/>
+    </SimpleForm>
+  </Edit>
+);
+
+export const UserCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="id" label="ID" allowEmpty="false"/>
+      <TextInput source="name" label="Nome" />
+      <TextInput source="email" label="Email" />
+    </SimpleForm>
+  </Create>
 );
