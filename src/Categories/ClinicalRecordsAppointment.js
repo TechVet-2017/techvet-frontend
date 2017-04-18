7fa125c3e6aa63d42b4fcf4aaa820838643030f2
leomeister <leomeister22@gmail.com>
2017-04-18 17:56:57 -0300

    Add parameters to appointments list
    Related to issue #16
    
    Signed-off-by: leomeister <leomeister22@gmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/Categories/ClinicalRecordsAppointment.js
+++ src/Categories/ClinicalRecordsAppointment.js
@@ -13,6 +13,33 @@ export const AppointmentRecordsList = (props) => (
 		<Datagrid>
 		    <TextField source="anamnesis" label="Anamnese" />
 	        <TextField source="veterinarian" label="Veterinário" />
+	        <TextField source="veterinarian" label="Veterinário" />
+			// <TextField source="clinicalProcedure" label="Procedimento Clínico" />
+			// <TextField source="observations" label="Observações" />
+			<TextField source="exam" label="Exame" />
+			// <TextField source="informations" label="Informações" />
+			// <TextField source="preliminaryMedication" label="Medicação Preliminar" />
+			// <TextField source="medicationDosage" label="Dosagem de Medicação" />
+			// <TextField source="medicationFrequency" label="Frequência de Medicação" />
+			// <TextField source="diagnostic" label="Diagnóstico" />
+			// <TextField source="prognostic" label="Prognóstico" />
+			// <TextField source="homeMedicationName" label="Nome da Medicação Domiciliar" />
+			// <TextField source="homeMedicationDosage" label="Dosagem da Medicação Domiciliar" />
+			// <TextField source="homeMedicationFrequency" label="Frequência da Medicação Domiciliar" />
+			// <TextField source="clinicMedicationName" label="Nome da Medicação Clínica" />
+			// <TextField source="clinicMedicationDosage" label="Dosagem da Medicação Clínica" />
+			// <TextField source="clinicMedicationFrequency" label="Frequência da Medicação Clínica" />
+
+			// <TextField source="clinicalHistory" label="aqui" />
+   			// <TextField source="diagnosis" label="aqui" />
+			// <TextField source="patientTemperature" label="aqui" />
+			// <TextField source="capillaryFill" label="aqui" />
+			// <TextField source="patientPulse" label="aqui" />
+			// <TextField source="mucosasApparent" label="aqui" />
+			// <TextField source="patientRespiratoryRate" label="aqui" />
+			// <TextField source="patientHeartRate" label="aqui" />
+			// <TextField source="patientWeight" label="aqui" />
+
 	        <EditButton />
 	        <DeleteButton />
 	    </Datagrid>
