9fa4cc4842644d2ac1064ae79e0061af130cbc80
leomeister <leomeister22@gmail.com>
2017-04-19 18:54:58 -0300

    Create edit method for Vaccination view.
    Related to issue #16
    
    Signed-off-by: leomeister <leomeister22@gmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/Categories/ClinicalRecordsVaccination.js
+++ src/Categories/ClinicalRecordsVaccination.js
@@ -5,6 +5,12 @@ import {
   EditButton,
   DeleteButton,
   List,
+  Edit,
+  DateInput,
+  TextInput,
+  SimpleForm,
+  LongTextInput,
+  NumberInput,
 } from 'admin-on-rest/lib/mui';
 
 export const VaccinationRecordsList = props => (
@@ -19,4 +25,31 @@ export const VaccinationRecordsList = props => (
       <DeleteButton />
     </Datagrid>
   </List>
+);
+
+export const VaccinationRecordsEdit = props => (
+  <Edit title="Ficha de Vacinação" {...props}>
+    <SimpleForm>
+      <DateInput source="vaccinationApplicationDate" label="Data da Vacinação" />
+      <TextInput source="vaccinationName" label="Vacina" />
+      <DateInput source="vaccinationReturnDate" label="Data de Retorno" />
+      <TextInput source="vaccinationLaboratory" label="Laboratório da Vacina" />
+      <DateInput source="vermifugationApplicationDate" label="Data de Vermifugação" />
+      <TextInput source="vermifugeName" label="Nome do Vermífugo" />
+      <TextInput source="vermifugeDosage" label="Dosagem do Vermífugo" />
+      <DateInput source="vermifugationReturnDate" label="Data de Retorno do Vermífugo" />
+
+      <LongTextInput source="anamnesis" label="Anamnese" />
+      <TextInput source="veterinarian" label="Veterinário" />
+      <TextInput source="clinicalHistory" label="Histórico Médico" />
+      <LongTextInput source="diagnosis" label="Diagnose?" />
+      <NumberInput source="patientTemperature" label="Temperatura do Paciente" />
+      <NumberInput source="capillaryFill" label="Nem Sei" />
+      <TextInput source="patientPulse" label="Pulso do Paciente" />
+      <TextInput source="mucosasApparent" label="Mucosas Aparentes" />
+      <NumberInput source="patientRespiratoryRate" label="Frequência Respiratória" />
+      <NumberInput source="patientHeartRate" label="Frequência Cardíaca" />
+      <NumberInput source="patientWeight" label="Peso" />
+    </SimpleForm>
+  </Edit>
 );
