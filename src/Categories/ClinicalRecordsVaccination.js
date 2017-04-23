85cbe65cc0d76c12151e26c89121d7605ab3167d
leomeister <leomeister22@gmail.com>
2017-04-23 14:16:55 -0300

    Make clinical reccord vaccination layout closer to it's prototype.
    Related to issue #16
    
    Signed-off-by: leomeister <leomeister22@gmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/Categories/ClinicalRecordsVaccination.js
+++ src/Categories/ClinicalRecordsVaccination.js
@@ -73,7 +73,7 @@ export const VaccinationRecordsCreate = props => (
         label="Atendido por Médico Veterinário"
         style={inlineStyle}
       />
-      <TextInput source="" label="Identificador" style={inlineStyle} />
+      <TextInput source="veterinarianIdentification" label="Identificador" style={inlineStyle} />
       <h2 style={title}>Anamnese</h2>
       <LongTextInput source="anamnesis" label="Histórico da Doença" />
       <h2 style={title}>Exame Físico</h2>
@@ -87,22 +87,47 @@ export const VaccinationRecordsCreate = props => (
         ]}
       />
       <NumberInput source="patientTemperature" label="Temperatura" />
-      <NumberInput source="patientHeartRate" label="TPC" />
-      <DateInput source="vaccinationApplicationDate" label="Data da Vacinação" />
-      <TextInput source="vaccinationName" label="Vacina" />
-      <DateInput source="vaccinationReturnDate" label="Data de Retorno" />
-      <TextInput source="vaccinationLaboratory" label="Laboratório da Vacina" />
-      <DateInput source="vermifugationApplicationDate" label="Data de Vermifugação" />
-      <TextInput source="vermifugeName" label="Nome do Vermífugo" />
-      <TextInput source="vermifugeDosage" label="Dosagem do Vermífugo" />
-      <DateInput source="vermifugationReturnDate" label="Data de Retorno do Vermífugo" />
-      <TextInput source="clinicalHistory" label="Histórico Médico" />
-      <LongTextInput source="diagnosis" label="Diagnose?" />
-      <NumberInput source="capillaryFill" label="Nem Sei" />
+      <NumberInput source="capillaryFill" label="TPC" />
       <TextInput source="patientPulse" label="Pulso do Paciente" />
-      <TextInput source="mucosasApparent" label="Mucosas Aparentes" />
+      <TextInput source="bloodPressure" label="PAS" />
+      <SelectInput
+        source="mucosasApparent"
+        label="Mucosas Aparentes"
+        choices={[
+        { id: 'normoCorada', name: 'Normo Corada' },
+        { id: '??', name: '??' },
+        { id: 'heh?', name: 'heh?' },  
+        ]}
+      />
       <NumberInput source="patientRespiratoryRate" label="Frequência Respiratória" />
+      <NumberInput source="patientHeartRate" label="Frequência Cardíaca" />
+      <h2 style={title}>Vacinação</h2>
+      <DateInput source="vaccinationApplicationDate" label="Data da Aplicação" />
+      <SelectInput
+        source="vaccinationName"
+        label="vacina"
+        choices={[
+         { id: 'vanguardPlusmonthly', name: 'Vanguard Plus Mensal' },
+         { id: 'vacina', name: 'Vacina' },
+         { id: 'nomeVacina', name: 'Nome de Vacina' },
+        ]}
+      />
+      <SelectInput
+        source="vaccinationLaboratory"
+        label="Laboratório"
+        choices={[
+         { id: 'pfizer', name: 'Pfizer' },
+         { id: 'dexter', name: 'Laboratório de Dexter' },
+         { id: 'sabin', name: 'Sabin' },
+        ]}
+      />
+      <DateInput source="vaccinationReturnDate" label="Data de Retorno" />
+      <h2 style={title}>Vermifugação</h2>
+      <DateInput source="vermifugationApplicationDate" label="Data da Aplicação" />
+      <TextInput source="vermifugeName" label="Vermífugo" />
       <NumberInput source="patientWeight" label="Peso" />
+      <TextInput source="vermifugeDosage" label="Dose" />
+      <DateInput source="vermifugationReturnDate" label="Data de Retorno" />
     </SimpleForm>
   </Create>
 );
