4771ef2f5ca6c5ac3046ccf620e46cb61db944e0
antoniocoj <_antonio_@hotmail.com>
2017-04-29 17:35:13 -0300

    Refactor CPF mask
    Related to issue #4
    Related to issue #2
    
    Signed-off-by: antoniocoj <_antonio_@hotmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>
    
    Signed-off-by: antoniocoj <_antonio_@hotmail.com>

--- src/Categories/Util/formatCPF.js
+++ src/Categories/Util/formatCPF.js
@@ -1,23 +1,45 @@
 export const formatCPF = (cpf, prevCpf) => {
-    if (!cpf) {
-        return cpf;
-    }
-    
+  let normalizedCpf = null;
+  if (cpf) {
+    normalizedCpf = cpf;
     const CPFNumbersOnly = cpf.replace(/[^\d]/g, '');
     if (!prevCpf || cpf.length > prevCpf.length) {
-        if (CPFNumbersOnly.length === 3)
-            return `${CPFNumbersOnly}.`;
-        if (CPFNumbersOnly.length === 6)
-            return `${CPFNumbersOnly.slice(0,3)}.${CPFNumbersOnly.slice(3,6)}.`;
-        if (CPFNumbersOnly.length === 9)
-            return `${CPFNumbersOnly.slice(0,3)}.${CPFNumbersOnly.slice(3,6)}.${CPFNumbersOnly.slice(6,9)}-`;
+      if (CPFNumbersOnly.length === 3) {
+        normalizedCpf = `${CPFNumbersOnly}.`;
+      } else {
+        // Nothing to do
+      }
+      if (CPFNumbersOnly.length === 6) {
+        normalizedCpf = `${CPFNumbersOnly.slice(0, 3)}.${CPFNumbersOnly.slice(3, 6)}.`;
+      } else {
+        // Nothing to do
+      }
+      if (CPFNumbersOnly.length === 9) {
+        normalizedCpf = `${CPFNumbersOnly.slice(0, 3)}.${CPFNumbersOnly.slice(3, 6)}.${CPFNumbersOnly.slice(6, 9)}-`;
+      } else {
+        // Nothing to do
+      }
+      if (CPFNumbersOnly.length === 11) {
+        normalizedCpf = `${CPFNumbersOnly.slice(0, 3)}.${CPFNumbersOnly.slice(3, 6)}.${CPFNumbersOnly.slice(6, 9)}-${CPFNumbersOnly.slice(9, 11)}`;
+      } else {
+        // Nothing to do
+      }
+    } else {
+      // Nothing to do
     }
-     if (CPFNumbersOnly.length <= 3)
-            return `${CPFNumbersOnly}`;
-        if (CPFNumbersOnly.length <= 6)
-            return `${CPFNumbersOnly.slice(0,3)}.${CPFNumbersOnly.slice(3)}`;
-        if (CPFNumbersOnly.length <= 9)
-            return `${CPFNumbersOnly.slice(0,3)}.${CPFNumbersOnly.slice(3,6)}.${CPFNumbersOnly.slice(6)}`;
-            
-    return `${CPFNumbersOnly.slice(0,3)}.${CPFNumbersOnly.slice(3,6)}.${CPFNumbersOnly.slice(6,9)}-${CPFNumbersOnly.slice(9,11)}`;
-}
+    if (CPFNumbersOnly.length <= 3) {
+      normalizedCpf = `${CPFNumbersOnly}`;
+    } else if (CPFNumbersOnly.length <= 6) {
+      normalizedCpf = `${CPFNumbersOnly.slice(0, 3)}.${CPFNumbersOnly.slice(3)}`;
+    } else if (CPFNumbersOnly.length <= 9) {
+      normalizedCpf = `${CPFNumbersOnly.slice(0, 3)}.${CPFNumbersOnly.slice(3, 6)}.${CPFNumbersOnly.slice(6)}`;
+    } else if (CPFNumbersOnly.length <= 11 || CPFNumbersOnly.length > 11) {
+      normalizedCpf = `${CPFNumbersOnly.slice(0, 3)}.${CPFNumbersOnly.slice(3, 6)}.${CPFNumbersOnly.slice(6, 9)}-${CPFNumbersOnly.slice(9, 11)}`;
+    } else {
+      // Nothing to do
+    }
+  } else {
+    normalizedCpf = cpf;
+  }
+  return normalizedCpf;
+};
