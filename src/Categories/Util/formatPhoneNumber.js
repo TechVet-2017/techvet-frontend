7e21144e9bbb4b1efd04411566521e84ae585fe0
antoniocoj <_antonio_@hotmail.com>
2017-04-29 18:00:48 -0300

    Refactor phone mask Related to issue #4 Related to issue #2
    Signed-off-by: antoniocoj <_antonio_@hotmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/Categories/Util/formatPhoneNumber.js
+++ src/Categories/Util/formatPhoneNumber.js
@@ -1,28 +1,39 @@
 export const formatPhoneNumber = (phone, previousPhone) => {
+  let normalizedPhoneNumber = null;
+  if (phone) {
+    normalizedPhoneNumber = phone;
     const phoneNumbersOnly = phone.replace(/[^\d]/g, '');
-    
     if (!previousPhone || phone.length > previousPhone.length) {
-        if (phoneNumbersOnly.length === 2) {
-            return `(${phoneNumbersOnly.slice(0,2)}) `;
-        }
-        if (phoneNumbersOnly.length === 6) {
-            return `(${phoneNumbersOnly.slice(0,2)}) ${phoneNumbersOnly.slice(2, 6)}-`;
-        }
-        if (phoneNumbersOnly.length === 10) {
-            return `(${phoneNumbersOnly.slice(0,2)}) ${phoneNumbersOnly.slice(2, 6)}-${phoneNumbersOnly.slice(6, 10)}`;
-        }        
-    }  
-    
+      if (phoneNumbersOnly.length === 2) {
+        normalizedPhoneNumber = `(${phoneNumbersOnly.slice(0, 2)}) `;
+      } else {
+        // Nothing to do
+      }
+      if (phoneNumbersOnly.length === 6) {
+        normalizedPhoneNumber = `(${phoneNumbersOnly.slice(0, 2)}) ${phoneNumbersOnly.slice(2, 6)}-`;
+      } else {
+        // Nothing to do
+      }
+      if (phoneNumbersOnly.length === 10) {
+        normalizedPhoneNumber = `(${phoneNumbersOnly.slice(0, 2)}) ${phoneNumbersOnly.slice(2, 6)}-${phoneNumbersOnly.slice(6, 10)}`;
+      } else {
+        // Nothing to do
+      }
+    }
+
     if (phoneNumbersOnly.length <= 2) {
-        return `${phoneNumbersOnly.slice(0)}`;
+      normalizedPhoneNumber = `${phoneNumbersOnly.slice(0)}`;
+    } else if (phoneNumbersOnly.length <= 6) {
+      normalizedPhoneNumber = `(${phoneNumbersOnly.slice(0, 2)}) ${phoneNumbersOnly.slice(2)}`;
+    } else if (phoneNumbersOnly.length <= 10) {
+      normalizedPhoneNumber = `(${phoneNumbersOnly.slice(0, 2)}) ${phoneNumbersOnly.slice(2, 6)}-${phoneNumbersOnly.slice(6)}`;
+    } else if (phoneNumbersOnly.length <= 11 || phoneNumbersOnly.length > 11) {
+      normalizedPhoneNumber = `(${phoneNumbersOnly.slice(0, 2)}) ${phoneNumbersOnly.slice(2, 7)}-${phoneNumbersOnly.slice(7, 11)}`;
+    } else {
+      // Nothing to do
     }
-    if (phoneNumbersOnly.length <= 6) {
-        return `(${phoneNumbersOnly.slice(0,2)}) ${phoneNumbersOnly.slice(2)}`;
-    }
-    if (phoneNumbersOnly.length <= 10) {
-        return `(${phoneNumbersOnly.slice(0,2)}) ${phoneNumbersOnly.slice(2, 6)}-${phoneNumbersOnly.slice(6)}`;
-    }
-    
-    return `(${phoneNumbersOnly.slice(0,2)}) ${phoneNumbersOnly.slice(2, 7)}-${phoneNumbersOnly.slice(7, 11)}`;
-    
-}
+  } else {
+    normalizedPhoneNumber = phone;
+  }
+  return normalizedPhoneNumber;
+};
