7829c55624742a848cf97515c4f5060d68b01179
gustavo2795 <gustavo.unb27@gmail.com>
2017-05-15 11:25:15 -0300

    Global variable declaration in BathAndGrooming
    Related to issue #2
    
    Signed-off-by: gustavo2795 <gustavo.unb27@gmail.com>

--- src/Categories/BathAndGrooming/BathAndGroomingEdit.js
+++ src/Categories/BathAndGrooming/BathAndGroomingEdit.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { DisabledInput, LongTextInput, Edit, SimpleForm } from 'admin-on-rest/lib/mui';
+// Global variables
+import { minimumServiceLength, maximumServiceLength } from './index';
 
 const validationIsEmpty = (values) => {
   const errors = {};
 
   if (!values.serviceBathGrooming) {
     errors.serviceBathGrooming = ['Campo obrigatório'];
-  } else if (values.serviceBathGrooming.length < 10) {
+  } else if (values.serviceBathGrooming.length < minimumServiceLength) {
     errors.serviceBathGrooming = ['Campo muito pequeno'];
-  } else if (values.serviceBathGrooming.length > 200) {
+  } else if (values.serviceBathGrooming.length > maximumServiceLength) {
     errors.serviceBathGrooming = ['Campo muito longo'];
   }
   return errors;
--- src/Categories/BathAndGrooming/index.js
+++ src/Categories/BathAndGrooming/index.js
@@ -1,3 +1,5 @@
 export { BathAndGroomingList } from './BathAndGroomingList';
 export { BathAndGroomingCreate } from './BathAndGroomingCreate';
 export { BathAndGroomingEdit } from './BathAndGroomingEdit';
+
+export const minimumServiceLength = 10;
