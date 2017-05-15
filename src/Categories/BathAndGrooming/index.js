1d494a0a8f537e423c8a44fad137de9d84c37f85
gustavo2795 <gustavo.unb27@gmail.com>
2017-05-15 11:30:46 -0300

    Add maximum Length of service in BathGrooming index
    Related to issue #2
    
    Signed-off-by: gustavo2795 <gustavo.unb27@gmail.com>

--- src/Categories/BathAndGrooming/index.js
+++ src/Categories/BathAndGrooming/index.js
@@ -2,4 +2,6 @@ export { BathAndGroomingList } from './BathAndGroomingList';
 export { BathAndGroomingCreate } from './BathAndGroomingCreate';
 export { BathAndGroomingEdit } from './BathAndGroomingEdit';
 
+// Global variables
 export const minimumServiceLength = 10;
+export const maximumServiceLength = 200;
