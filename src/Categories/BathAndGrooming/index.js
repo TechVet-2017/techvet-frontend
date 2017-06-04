54e917b833f6d71efa5cd05ddd4cacede32d6b5f
Mateus Vieira <mateusvroriz6b@gmail.com>
2017-06-04 11:07:38 -0300

    Fix the index code
    Related to issue #14 #2
    
    Signed-off-by: Mateus Vieira <mateusvroriz6b@gmail.com>
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>
    Signed-off-by: Luiz Guilherme Silva <do.guilherme@hotmail.com

--- src/Categories/BathAndGrooming/index.js
+++ src/Categories/BathAndGrooming/index.js
@@ -1,7 +1,3 @@
 export { BathAndGroomingList } from './BathAndGroomingList';
 export { BathAndGroomingCreate } from './BathAndGroomingCreate';
 export { BathAndGroomingEdit } from './BathAndGroomingEdit';
-
-// Global variables
-export const minimumServiceLength = 10;
-export const maximumServiceLength = 200;
