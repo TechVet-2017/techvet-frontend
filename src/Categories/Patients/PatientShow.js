d94d3819df177bed7954e08a4482813ed7df2dd4
gustavo2795 <gustavo.unb27@gmail.com>
2017-06-11 23:14:27 -0300

    Add new IdOwner field in patient
    Related to issue #27
    
    Signed-off-by: varley silva <varleysilva@me.com>
    Signed-off-by: Adailson <adailson2@gmail.com>
    Signed-off-by: gustavo2795 <gustavo.unb27@gmail.com>

--- src/Categories/Patients/PatientShow.js
+++ src/Categories/Patients/PatientShow.js
@@ -4,11 +4,15 @@ import {
   SimpleShowLayout,
   DateField,
   TextField,
+  ReferenceField,
 } from 'admin-on-rest/lib/mui';
 
 export const PatientShow = props => (
   <Show title={'Cadastro do Paciente'} {...props}>
     <SimpleShowLayout>
+      <ReferenceField label="Dono" source="ownerId" reference="owners">
+        <TextField label="Dono" source="ownerName" />
+      </ReferenceField>
       <TextField
         value="off"
         source="patientName"
