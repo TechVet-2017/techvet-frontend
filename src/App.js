aa5a370483b0a652e387168a217cf422faa9659a
antoniocoj <_antonio_@hotmail.com>
2017-06-24 20:47:02 -0300

    Removing unused menu itens
    Signed-off-by: antoniocoj <_antonio_@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -12,15 +12,10 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import { Delete } from 'admin-on-rest/lib/mui';
-import { PostList, PostEdit, PostCreate } from './Categories/Example';
 import { UserList, UserEdit, UserCreate } from './Categories/Users';
 import { OwnerList, OwnerShow, OwnerCreate, OwnerEdit } from './Categories/Owners';
 import { PatientList, PatientEdit, PatientCreate, PatientShow, PatientFilter } from './Categories/Patients';
-import { AttendenceList } from './Categories/Attendences';
-import { HospitalizationList } from './Categories/Hospitalizations';
-import { VaccinationScheduleList } from './Categories/VaccinationSchedule';
 import { BathAndGroomingList, BathAndGroomingEdit, BathAndGroomingCreate, BathAndGroomingShow } from './Categories/BathAndGrooming';
-import { FinancialReportList } from './Categories/FinancialReports';
 import { AppointmentRecordsList, AppointmentRecordsEdit, AppointmentRecordsCreate } from './Categories/ClinicalRecordsAppointment';
 import { VaccinationRecordsList, VaccinationRecordsEdit, VaccinationRecordsCreate } from './Categories/ClinicalRecordsVaccination';
 import Dashboard from './Dashboard';
@@ -36,10 +31,15 @@ const App = () => (
     dashboard={Dashboard}
   >
     <Resource
-      name="post" options={{ label: 'Exemplo' }} list={PostList} edit={PostEdit}
-      create={PostCreate} remove={Delete} icon={PostIcon}
+      name="owners"
+      options={{ label: 'Proprietários' }}
+      list={OwnerList}
+      show={OwnerShow}
+      edit={OwnerEdit}
+      create={OwnerCreate}
+      remove={Delete}
+      icon={OwnerIcon}
     />
-
     <Resource
       name="patients"
       options={{ label: 'Pacientes' }}
@@ -52,30 +52,21 @@ const App = () => (
       icon={PatientIcon}
     />
     <Resource
-      name="users"
-      options={{ label: 'Usuários' }}
-      list={UserList}
-      edit={UserEdit}
-      create={UserCreate}
+      name="appointmentRecords"
+      options={{ label: 'Consultas' }}
+      list={AppointmentRecordsList}
+      edit={AppointmentRecordsEdit}
+      create={AppointmentRecordsCreate}
       remove={Delete}
-      icon={UserIcon}
+      icon={AttendenceIcon}
     />
     <Resource
-      name="owners"
-      options={{ label: 'Proprietários' }}
-      list={OwnerList}
-      show={OwnerShow}
-      edit={OwnerEdit}
-      create={OwnerCreate}
+      name="vaccinationRecords"
+      options={{ label: 'Vacinação' }}
+      list={VaccinationRecordsList}
+      edit={VaccinationRecordsEdit}
+      create={VaccinationRecordsCreate}
       remove={Delete}
-      icon={OwnerIcon}
-    />
-    <Resource name="attendence" options={{ label: 'Consultas' }} list={AttendenceList} icon={AttendenceIcon} />
-    <Resource name="hospitalization" options={{ label: 'Internações' }} list={HospitalizationList} icon={HospitalizationIcon} />
-    <Resource
-      name="vaccinationSchedule" options={{ label: 'Calendário de Vacinação' }}
-      list={VaccinationScheduleList}
-      icon={VaccinationScheduleIcon}
     />
     <Resource
       name="bathAndGrooming"
@@ -87,9 +78,15 @@ const App = () => (
       remove={Delete}
       icon={BathAndGroomingIcon}
     />
-    <Resource name="financialReport" options={{ label: 'Relatório Financeiro' }} list={FinancialReportList} icon={FinancialReportIcon} />
-    <Resource name="appointmentRecords" options={{ label: 'Consultas' }} list={AppointmentRecordsList} edit={AppointmentRecordsEdit} create={AppointmentRecordsCreate} remove={Delete} icon={AttendenceIcon} />
-    <Resource name="vaccinationRecords" options={{ label: 'Vacinação' }} list={VaccinationRecordsList} edit={VaccinationRecordsEdit} create={VaccinationRecordsCreate} remove={Delete} />
+    <Resource
+      name="users"
+      options={{ label: 'Usuários' }}
+      list={UserList}
+      edit={UserEdit}
+      create={UserCreate}
+      remove={Delete}
+      icon={UserIcon}
+    />
   </Admin>
 );
 
